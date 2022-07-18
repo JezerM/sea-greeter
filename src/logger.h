39d35cb3c4f89486287971357d985e3dc48ce25d
Jezer Mejía <amyuki4@icloud.com>
2022-07-17 22:29:16 -0600

    Allow printf like formatted messages in logger.h

--- src/logger.h
+++ src/logger.h
@@ -7,15 +7,17 @@
 #include <glib.h>
 #include <locale.h>
 
-#define logger_raw(type, message) {\
+#define logger_raw(type, message, ...) {\
   GDateTime *now = g_date_time_new_now_local();\
   char *timestamp = g_date_time_format(now, "%Y-%m-%d %H:%M:%S");\
+  char *str = g_strdup_printf(message, ##__VA_ARGS__);\
   fprintf(stderr, "%s [ %s ] %s %d: %s\n",\
-      timestamp, type, g_path_get_basename(__FILE__), __LINE__, message);\
+      timestamp, type, g_path_get_basename(__FILE__), __LINE__, str);\
+  g_free(str);\
 }
 
-#define logger_debug(message) { logger_raw("DEBUG", message) }
-#define logger_error(message) { logger_raw("ERROR", message) }
-#define logger_warn(message) { logger_raw("WARN", message) }
+#define logger_debug(message, ...) { logger_raw("DEBUG", message, ##__VA_ARGS__) }
+#define logger_error(message, ...) { logger_raw("ERROR", message, ##__VA_ARGS__) }
+#define logger_warn(message, ...) { logger_raw("WARN", message, ##__VA_ARGS__) }
 
 #endif
