95b6d3e98319b67b78cd2f6771b5f167e3d40f3e
Jezer Mejía <amyuki4@icloud.com>
2022-07-18 21:16:50 -0600

    Improved zoom-in and zoom-out actions with predefined steps. Added these actions to menu_bar.

--- src/browser-commands.c
+++ src/browser-commands.c
@@ -3,6 +3,39 @@
 #include "browser-commands.h"
 #include "browser.h"
 
+static float zoom_steps[] = {
+  0.30f, 0.50f, 0.75f, 0.85f, 1.00f,
+
+  1.15f, 1.25f, 1.50f, 1.75f,
+
+  2.00f, 2.50f, 3.00f,
+};
+
+float
+get_zoom_step_level(float current_level, int steps)
+{
+  float new_level = 0;
+  int near_index = 0;
+  int steps_count = G_N_ELEMENTS(zoom_steps);
+
+  for (int i = 0; i < steps_count; i++) {
+    if (current_level < zoom_steps[i])
+      break;
+    near_index = i;
+  }
+  int expected_index = near_index + steps;
+
+  if (expected_index > steps_count - 1) {
+    new_level = zoom_steps[steps_count - 1];
+  } else if (expected_index <= 0) {
+    new_level = zoom_steps[0];
+  } else {
+    new_level = zoom_steps[expected_index];
+  }
+
+  return new_level;
+}
+
 /*
  * Toggle web view inspector
  */
@@ -31,9 +64,9 @@ browser_set_zoom(Browser *browser, int zoom)
   WebKitWebView *web_view = WEBKIT_WEB_VIEW(browser->web_view);
   double current_zoom = webkit_web_view_get_zoom_level(web_view);
   if (zoom == BROWSER_ZOOM_IN) {
-    current_zoom += 0.1;
+    current_zoom = get_zoom_step_level(current_zoom, 1);
   } else if (zoom == BROWSER_ZOOM_OUT) {
-    current_zoom -= 0.1;
+    current_zoom = get_zoom_step_level(current_zoom, -1);
   }
   webkit_web_view_set_zoom_level(web_view, current_zoom);
 }
